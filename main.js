a1df37ae4462d66dea474d128be7739cc932d26a
rahadsr30 <rahadahmedsr30@gmail.com>
2023-09-13 14:44:45 +0600

    js add

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-var typed = new typed(".text", {
-  strings: ["Fontent Developer", "Youtuber", "Web Developer"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
-});
+console.log("hellos");
